aef66e2c1ce882f28056a37f5ae4f78642447db1
Troy Spencer <troy.a.spencer@gmail.com>
2018-06-11 21:30:42 -0500

    Fixed default service deployment

--- server/dev.bat
+++ server/dev.bat
@@ -1,1 +1,1 @@
-python27 "%userprofile%/AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin/dev_appserver.py" gaefiles/datagrinder/app.yaml
+python27 "%userprofile%/AppData/Local/Google/Cloud SDK/google-cloud-sdk/bin/dev_appserver.py" gaefiles/main/app.yaml
