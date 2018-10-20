abcad34007d53cbb1266a46795c19f2db1a91b8a
troyspencer <troy.a.spencer@gmail.com>
2018-10-20 14:42:39 -0500

    Initial conversion of GAE to go1.11

--- server/gaefiles/main/serve.go
+++ server/gaefiles/main/serve.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"gcloud/grpc_playground/server"
-
-	"google.golang.org/appengine"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 	server.Run()
 
-	appengine.Main()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Printf("Defaulting to port %s", port)
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
